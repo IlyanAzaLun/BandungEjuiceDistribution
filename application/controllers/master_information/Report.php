bc85e208b8eb0235cfedb3792981585bdb7a93d9
Ilyan Aza Lun <iyang_agung_s@hotmail.com>
2022-10-10 21:08:58 +0700

    update

--- application/controllers/master_information/Report.php
+++ application/controllers/master_information/Report.php
@@ -435,7 +435,7 @@ class Report extends MY_Controller
         $sheet->setCellValue("U1", "sub_district");
         $sheet->setCellValue("V1", "city");
         $sheet->setCellValue("W1", "province");
-        $sheet->setCellValue("Z1", "zip");
+        $sheet->setCellValue("X1", "zip");
         $sheet->setCellValue("Y1", "contact_phone");
         $sheet->setCellValue("Z1", "contact_mail");
         $sheet->setCellValue("AA1", "user_created");
@@ -465,7 +465,7 @@ class Report extends MY_Controller
             $sheet->setCellValue("U".$i, $value->sub_district);
             $sheet->setCellValue("V".$i, $value->city);
             $sheet->setCellValue("W".$i, $value->province);
-            $sheet->setCellValue("Z".$i, $value->zip);
+            $sheet->setCellValue("X".$i, $value->zip);
             $sheet->setCellValue("Y".$i, $value->contact_phone);
             $sheet->setCellValue("Z".$i, $value->contact_mail);
             $sheet->setCellValue("AA".$i, $value->user_created);
